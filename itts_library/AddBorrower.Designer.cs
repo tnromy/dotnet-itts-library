3c7b55229f3ff0961c5ef80b22cebcd3af8015f4
tnromy <tuanromy@gmail.com>
2023-11-24 12:20:28 +0700

    menambahkan action insert ke table user jika form telah diisi dan username belum digunakan

--- itts_library/AddBorrower.Designer.cs
+++ itts_library/AddBorrower.Designer.cs
@@ -33,6 +33,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.full_name = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.full_name_input = new System.Windows.Forms.TextBox();
+            this.username_input = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.password_input = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.add_button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // role_level
@@ -60,9 +67,9 @@
             this.label2.Location = new System.Drawing.Point(10, 76);
             this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.Size = new System.Drawing.Size(120, 13);
             this.label2.TabIndex = 12;
-            this.label2.Text = "Daftar akun peminjam:";
+            this.label2.Text = "Tambah akun peminjam";
             // 
             // full_name
             // 
@@ -83,11 +90,81 @@
             this.label1.TabIndex = 10;
             this.label1.Text = "ITTSLIB";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 106);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Nama Lengkap:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // full_name_input
+            // 
+            this.full_name_input.Location = new System.Drawing.Point(97, 106);
+            this.full_name_input.Name = "full_name_input";
+            this.full_name_input.Size = new System.Drawing.Size(100, 20);
+            this.full_name_input.TabIndex = 16;
+            // 
+            // username_input
+            // 
+            this.username_input.Location = new System.Drawing.Point(97, 150);
+            this.username_input.Name = "username_input";
+            this.username_input.Size = new System.Drawing.Size(100, 20);
+            this.username_input.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 150);
+            this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Username:";
+            // 
+            // password_input
+            // 
+            this.password_input.Location = new System.Drawing.Point(97, 194);
+            this.password_input.Name = "password_input";
+            this.password_input.PasswordChar = '*';
+            this.password_input.Size = new System.Drawing.Size(100, 20);
+            this.password_input.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 194);
+            this.label5.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Password:";
+            // 
+            // add_button
+            // 
+            this.add_button.Location = new System.Drawing.Point(122, 250);
+            this.add_button.Name = "add_button";
+            this.add_button.Size = new System.Drawing.Size(75, 23);
+            this.add_button.TabIndex = 21;
+            this.add_button.Text = "Tambah";
+            this.add_button.UseVisualStyleBackColor = true;
+            this.add_button.Click += new System.EventHandler(this.add_button_Click);
+            // 
             // AddBorrower
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.add_button);
+            this.Controls.Add(this.password_input);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.username_input);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.full_name_input);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.role_level);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label2);
@@ -107,5 +184,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label full_name;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox full_name_input;
+        private System.Windows.Forms.TextBox username_input;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox password_input;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button add_button;
     }
 }
--- itts_library/AddBorrower.cs
+++ itts_library/AddBorrower.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BCrypt.Net;
 
 namespace itts_library
 {
@@ -29,6 +30,48 @@ namespace itts_library
                 role_level.Text = "Peminjam";
 
             }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_button_Click(object sender, EventArgs e)
+        {
+            DataClassesDataContext dc = new DataClassesDataContext();
+
+            string inputUsername = username_input.Text;
+
+            // Cek apakah sudah ada user dengan username yang sama
+            var existingUser = dc.users.FirstOrDefault(u => u.username == inputUsername);
+
+            if (existingUser == null)
+            {
+                // Jika belum ada, tambahkan user baru
+                itts_library.user userInput = new itts_library.user
+                {
+                    full_name = full_name_input.Text,
+                    username = inputUsername,
+                    password = BCrypt.Net.BCrypt.EnhancedHashPassword(password_input.Text, 11),
+                    role_level = 2
+                };
+
+                // Tambahkan user baru ke tabel
+                dc.users.InsertOnSubmit(userInput);
+                dc.SubmitChanges();
+
+                // Informasi bahwa user telah ditambahkan
+                Borrowers borrowers = new Borrowers();
+                borrowers.Show();
+                this.Hide();
+            }
+            else
+            {
+                // Jika sudah ada, berikan pesan kesalahan atau ambil tindakan yang sesuai
+                Console.WriteLine("Username sudah digunakan. Pilih username lain.");
+            }
+
         }
     }
 }
